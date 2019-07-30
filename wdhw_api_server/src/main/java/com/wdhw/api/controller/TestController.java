00195246eaf96482aaf6a650037bd6838929e87e
wangff <1024904798@qq.com>
2019-07-30 11:06:54 +0800

    nacos

--- wdhw_api_server/src/main/java/com/wdhw/api/controller/TestController.java
+++ wdhw_api_server/src/main/java/com/wdhw/api/controller/TestController.java
@@ -3,7 +3,6 @@ package com.wdhw.api.controller;
 import com.wdhw.api.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
---
+++ wdhw_zuul_server/src/main/java/com/wdhw/api/WdhwZuulServerApplication.java
@@ -0,0 +1,41 @@
+
+package com.wdhw.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Slf4j
+@ComponentScan(basePackages = "com.wdhw")
+@EnableAspectJAutoProxy
+@RefreshScope
+@EnableZuulProxy
+@SpringCloudApplication
+public class WdhwZuulServerApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(WdhwZuulServerApplication.class);
+    }
+
+    public void run(String... args) {
+        log.info("WdhwEurekaServerApplication started...");
+    }
+    /*
+    请求前拦截，检查Token信息
+     */
+    /*@Bean
+    public BeforeRequestFilter beforeRequestFilter() {
+        return new BeforeRequestFilter();
+    }*/
+    /*
+    请求后，对数据进行jsonP输出
+     */
+ /*   @Bean
+    public EndRequestFilter endRequestFilter(){
+        return new EndRequestFilter();
+    }*/
+}
