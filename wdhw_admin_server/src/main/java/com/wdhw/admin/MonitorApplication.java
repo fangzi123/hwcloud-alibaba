a556e3bf2a7ad187cc6d5fdc400ea1284edad8cf
wangff <1024904798@qq.com>
2019-08-02 14:12:11 +0800

    nacos

--- wdhw_admin_server/src/main/java/com/wdhw/admin/MonitorApplication.java
+++ wdhw_admin_server/src/main/java/com/wdhw/admin/MonitorApplication.java
@@ -3,6 +3,7 @@ package com.wdhw.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
